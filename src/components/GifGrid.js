c02c3dd89d8b234a7f9e1865dca24738feec727c
Santiago Pegels <santiagopegels@gmail.com>
2021-05-27 11:32:16 -0300

    add GifGridItem

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
+
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        getGifs();
+    }, [])
 
     const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=PSUGwbdVl9svnHv2IaKGJ4ukTLdZSDhP'
@@ -14,14 +21,20 @@ export const GifGrid = ({ category }) => {
                 url: img.images?.downsized_medium.url
             }
         })
-        console.log(gifs);
+        setImages(gifs);
     }
-
-    getGifs();
 
     return (
         <>
             <h3>{category}</h3>
+            {
+                images.map((img) => (
+                    <GifGridItem 
+                    key={img.id}
+                    {...img}
+                    />
+                ))
+            }
         </>
     )
 }
