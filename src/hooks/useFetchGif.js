f607cfef94b1ef05dcbd85de194d17cae447085b
Santiago Pegels <santiagopegels@gmail.com>
2021-05-31 15:01:42 -0300

    Add hook test when is updated

--- src/hooks/useFetchGif.js
+++ src/hooks/useFetchGif.js
@@ -17,7 +17,7 @@ export const useFetchGif = (category) => {
                     data: imgs,
                     loading: false
                 })
-            }, 800)
+            }, 100)
 
         })
     }, [category])
--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,15 +2,31 @@ import { useFetchGif } from "../../hooks/useFetchGif"
 import { renderHook } from '@testing-library/react-hooks'
 
 describe('Test on hook useFetchGifs', () => {
-    test('should return the initial state', () => {
+    const category = 'One Punch'
 
-        const category = 'One Punch'
-        const {result} = renderHook(() => useFetchGif(category))
+    test('should return the initial state', async () => {
+
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGif(category));
+
         const { data, loading } = result.current
+        await waitForNextUpdate();
 
-        expect( data ).toEqual([])
-        expect( loading ).toBe(true)
+        expect(data).toEqual([])
+        expect(loading).toBe(true)
 
     })
+
+    test('should return an images array and loading false', async () => {
+
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGif(category));
+
+        await waitForNextUpdate();
+        const { data, loading } = result.current
+
+        expect(data.length).toBe(10)
+        expect(loading).toBe(false)
+
+    })
+
 
 })
